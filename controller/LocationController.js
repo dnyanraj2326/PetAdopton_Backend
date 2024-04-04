cf331f7f3b739b095e8f0c24584a12358f193bd7
Dnyanraj <apple@vcx.local>
2024-04-04 22:47:42 +0530

    response changes

--- controller/LocationController.js
+++ controller/LocationController.js
@@ -1,23 +1,13 @@
-const model = require("../model/Location");
-const Locations = model.Locations;
+const model = require('../model/Location')
+const Locations = model.Locations
 
-exports.getAllLocations = async (req, res) => {
-  const locationData = await Locations.find();
-  res.json(locationData);
-};
+exports.getAllLocations = async (req,res) => {
+    const locationData = await Locations.find()
+    res.json(locationData)
+}
 
-exports.createLocation = async (req, res) => {
-    try {
-        const locations = new Locations(req.body);
-        const result = await locations.save();
-        res
-          .status(201)
-          .json(result, {
-            status: "201",
-            message: "Location created successfully",
-          });
-      } catch (error) {
-        console.error('Error creating location:', error);
-        res.status(500).json({ error: 'Failed to create location' }); // Generic error for now, provide more details if possible
-      }
-};
+exports.createLocation = async (req,res) => {
+    const locations = new Locations(req.body);
+    const result = await locations.save() 
+    res.status(201).json(result)
+}
