5267c95d2dfa061a3c2651ac0fddbf6e0b9ee788
Dnyanraj <apple@vcx.local>
2024-04-04 22:38:15 +0530

    response changes

--- controller/LocationController.js
+++ controller/LocationController.js
@@ -1,13 +1,18 @@
-const model = require('../model/Location')
-const Locations = model.Locations
+const model = require("../model/Location");
+const Locations = model.Locations;
 
-exports.getAllLocations = async (req,res) => {
-    const locationData = await Locations.find()
-    res.json(locationData)
-}
+exports.getAllLocations = async (req, res) => {
+  const locationData = await Locations.find();
+  res.json(locationData);
+};
 
-exports.createLocation = async (req,res) => {
-    const locations = new Locations(req.body);
-    const result = await locations.save() 
-    res.status(201).json(result)
-}
+exports.createLocation = async (req, res) => {
+  const locations = new Locations(req.body);
+  const result = await locations.save();
+  res
+    .status(201)
+    .json(result, {
+      status: "201",
+      message: "locations send on server successfully",
+    });
+};
