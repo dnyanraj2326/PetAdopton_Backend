fb0cbc4a57bbb3ea1bcb4fa064f6acd17e2a5d7f
Dnyanraj <apple@vcx.local>
2024-04-04 22:42:03 +0530

    response changes

--- controller/LocationController.js
+++ controller/LocationController.js
@@ -7,12 +7,17 @@ exports.getAllLocations = async (req, res) => {
 };
 
 exports.createLocation = async (req, res) => {
-  const locations = new Locations(req.body);
-  const result = await locations.save();
-  res
-    .status(201)
-    .json(result, {
-      status: "201",
-      message: "locations send on server successfully",
-    });
+    try {
+        const locations = new Locations(req.body);
+        const result = await locations.save();
+        res
+          .status(201)
+          .json(result, {
+            status: "201",
+            message: "Location created successfully",
+          });
+      } catch (error) {
+        console.error('Error creating location:', error);
+        res.status(500).json({ error: 'Failed to create location' }); // Generic error for now, provide more details if possible
+      }
 };
